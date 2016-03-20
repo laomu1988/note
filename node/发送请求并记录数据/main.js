866fa4c3322a231782597a798ce0de1af75059b1
zhilong mu <laomu1988@qq.com>
2016-03-20 21:07:56 +0800

    request请求返回数据

---
+++ node/发送请求并记录数据/main.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var path = require('path');
+var fs = require('fs');
+
+var src = 'http://dev.list.leanapp.cn/api/project/list?finished=false&removed=false';
+
+console.log(path.parse(src));
+
+var fileName = 'test.json';
+
+// 直接请求并且写入文件
+request(src).pipe(fs.createWriteStream(fileName));
+request(src, function (error, response, body) {
+    if (response.statusCode == 201) {
+        console.log('document saved as: http://mikeal.iriscouch.com/testjs/' + rand)
+    } else {
+        console.log('error: ' + response.statusCode)
+        console.log(body)
+    }
+}).pipe(fs.createWriteStream(fileName));
